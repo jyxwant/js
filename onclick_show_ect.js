1ad8d8e207a0ad4e2c4b478efe87aadd2d4699b8
jyx <myemailforstudy@gmail.com>
2018-04-01 20:38:50 +0800

    18-4-1-20.38

---
+++ onclick_show_ect.js
@@ -0,0 +1,8 @@
+
+function open_new(){
+    window.open('dom.html','hi window','width=320,height=400"')
+}
+
+function open_this(href_string){
+    window.open(href_string,'hi window','width=320,height=400"')
+}
