785b8100e2309169c7126c5e34a49496d1090922
jyxwant <1132346005@qq.com>
2018-07-14 20:09:04 +0800

    7-14

--- 2018-7/js/table.js
+++ 2018-7/js/table.js
@@ -5,7 +5,6 @@ init_table=function(){
     if(!document.getElementById("table")) return false;
     var add = document.getElementById("add");
     var table = document.getElementById("table");
-    var a = document.getElementById("tr");
     add.onclick = function(){
         var new_tr = document.createElement("tr");
         var time_td = document.createElement("td");
@@ -20,15 +19,81 @@ init_table=function(){
         new_tr.appendChild(time_td);
         new_tr.appendChild(plan_td);
         new_tr.appendChild(finsh_td);
-        table.children[0].appendChild(new_tr);
+        var loadrow = document.getElementById("loadrow");
+        table.children[0].insertBefore(new_tr,loadrow);
     }
-
-    
-    
 };
+
+function addloadrow(){   //添加初始化行    + | 内容 | 完成情况
+    var loadrow = document.createElement("tr");
+    var pluscol = document.createElement("td");
+    var plancol = document.createElement("td");
+    var finishcol = document.createElement("td");
+    loadrow.id = "loadrow";
+
+    pluscol.colSpan = "1";
+    var plus_a = document.createElement("a");
+    plus_a.innerText = "+";
+    plus_a.href = "#";
+    plus_a.onclick = function(){
+        addworkrow();
+        return false;
+    } //添加工作行
+    pluscol.appendChild(plus_a);
+    loadrow.appendChild(pluscol);
+
+    plancol.colSpan = "24";
+    plancol.innerText = "计划内容"
+    loadrow.appendChild(plancol);
+
+    finishcol.colSpan = "1";
+    finishcol.innerText = "完成情况"
+    loadrow.appendChild(finishcol);
+
+    var table = document.getElementById("table");
+    table.children[0].appendChild(loadrow);
+}
+
+function addworkrow(){  //正式的计划行     日期 | 计划内容 | 完成情况
+    var workrow = document.createElement("tr");
+    var timecol = document.createElement("td");
+    var plancol = document.createElement("td");
+    var finishcol = document.createElement("td");
+
+
+    timecol.colSpan = "1";
+    var time_a = document.createElement("a");
+    time_a.innerText = "日期";
+    //time_a.onclick =  添加一个正式的行
+    timecol.appendChild(time_a);
+    workrow.appendChild(timecol);
+
+    plancol.colSpan = "24";
+    var plan_a = document.createElement("a");
+    plan_a.innerText = "计划内容";
+    plan_a.href = "#";
+    plan_a.onclick = function(){
+        window.open("plan.html","","height=400,width=600,resizable=no")
+    }
+    plancol.appendChild(plan_a);
+    workrow.appendChild(plancol);
+
+    finishcol.colSpan = "1";
+    var finish_a = document.createElement("a");
+    finish_a.innerText = "完成情况";
+    //finish_a.onclick =  添加一个正式的行
+    finishcol.appendChild(finish_a);
+    workrow.appendChild(finishcol);
+
+    var loadrow = document.getElementById("loadrow");
+    var table = document.getElementById("table").children[0];
+    table.insertBefore(workrow,loadrow);
+}
+
+
 function addonlaod(func){
     var check=window.onload
-    if(typeof check!='function'){
+    if(typeof check!="function"){
         window.onload=func
     }
     else{
@@ -40,3 +105,4 @@ function addonlaod(func){
     
 }
 addonlaod(init_table);
+addonlaod(addloadrow);
